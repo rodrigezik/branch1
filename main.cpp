1071149fa30ce0c19d46b50ed469b9fbda76f51d
rodrigezik <rodrigezcoding@gmail.com>
2026-08-01 13:39:54 +0200

    + modified main + new file

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//its funny im using std namespace aint it?
+//i hate writing new repo's...
+
+int add_numberCLC1;
+int add_numberCLC2;
+
+int main(){
+
+    
+}
---
+++ void_functions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void CLCmenu_interact() {
+
+    cout<<R"( WELCOME TO CALCULATOR!
+    [1] ADD [2] MINUS 
+    [3] MULTIPLY [3] DIVIDE
+    )"<<endl;
+}
+
+void CLCadd_choice() {
+    
+}
